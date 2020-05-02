df7fd3fc65a4f65abbe282980eaaed78467f5b3b
Puneeth Nikin <nikin.2492@gmail.com>
2020-05-02 09:02:05 +0100

    added all models

--- code.R
+++ code.R
@@ -42,25 +42,6 @@ library(lubridate)
 library(caret)
 library(tidyverse)
 library(data.table)
-# extract year released from  title
-pattern_year_released<-"(\\d{4})\\)$"
-edx<-edx%>%mutate(year_released= as.numeric(str_match(title,pattern_year_released)[,2]))
-#convert timestamp to readable datetime and extract year rated
-edx<-edx%>%mutate(date_time_rated=as.POSIXct(timestamp, origin="1970-01-01"),year_rated=year(date_time_rated))
-edx<-edx%>%mutate(movieId=as.factor(movieId),userId=as.factor(userId))
-
-# calculating average user rating and average movie rating
-avg_user_rating_df<-edx%>%group_by(userId)%>%summarise(avg_user_rating=round(mean(rating),digits=3))
-edx<-edx%>%left_join(avg_user_rating_df,by="userId")
-avg_movie_rating_df<-edx%>%group_by(movieId)%>%summarise(avg_movie_rating=round(mean(rating),digits=3))
-edx<-edx%>%left_join(avg_movie_rating_df,by="movieId")
-
-#partition edx dataset
-set.seed(1, sample.kind="Rounding")
-# if using R 3.5 or earlier, use `set.seed(1)` instead
-test_index_edx <- createDataPartition(y = edx$rating, times = 1, p = 0.5, list = FALSE)
-test_set <- edx[test_index_edx,]
-train_set <- edx[-test_index_edx,]
 
 #RMSE function
 RMSE<-function(predicted_ratings,actual_ratings){
@@ -68,7 +49,43 @@ RMSE<-function(predicted_ratings,actual_ratings){
 }
 
 
-fit<- train(rating~avg_movie_rating+avg_user_rating+year_released+year_rated,data = train_set,method='rf')
-y_hat<-predict(fit,test_set)
+# extract year released from  title
+pattern_year_released<-"(\\d{4})\\)$"
+edx<-edx%>%mutate(year_released= as.numeric(str_match(title,pattern_year_released)[,2]))
+#convert timestamp to readable datetime and extract year rated
+edx<-edx%>%mutate(date_time_rated=as.POSIXct(timestamp, origin="1970-01-01"),year_rated=year(date_time_rated))
+edx<-edx%>%mutate(movieId=as.factor(movieId),userId=as.factor(userId))
 
-RMSE(y_hat,test_set$rating)
+#create a column for each genre and check if movie in genre
+genres_list<-unique(unlist(strsplit(as.vector(unique(edx$genres)),'\\|')))
+x<-map(genres_list,function(x){
+  str_detect(edx$genres,x)
+})
+View(edx)
+# calculating average user rating and average movie rating
+avg_user_rating_df<-edx%>%group_by(userId)%>%summarise(avg_user_rating=round(mean(rating),digits=3))
+edx<-edx%>%left_join(avg_user_rating_df,by="userId")
+avg_movie_rating_df<-edx%>%group_by(movieId)%>%summarise(avg_movie_rating=round(mean(rating),digits=3))
+edx<-edx%>%left_join(avg_movie_rating_df,by="movieId")
+
+#As the dataset is very large it take a long time to train and fit models. Hence I take a smaller sample for building the model.
+#dim(edx)
+#set.seed(1,sample.kind = "Rounding")
+#edx_sample<- edx[sample(nrow(edx),5000),]
+
+#partition edx dataset
+set.seed(1, sample.kind="Rounding")
+# if using R 3.5 or earlier, use `set.seed(1)` instead
+test_index_edx <- createDataPartition(y = edx$rating, times = 1, p = 0.5, list = FALSE)
+test_set <- edx[test_index_edx,]
+train_set <- edx[-test_index_edx,]
+#making sure test set has same movies as train set
+test_set<- test_set%>%semi_join(train_set,by='movieId')%>%semi_join(train_set,by='userId')
+
+control<-trainControl(method = "cv", number = 10, p = .9)
+
+fit<- train(rating~avg_movie_rating+avg_user_rating+year_released+year_rated,data = train_set,method='lm',trControl=control)
+
+#y_hat<-predict(fit,test_set_sample)
+
+#RMSE(y_hat,test_set_sample$rating)
